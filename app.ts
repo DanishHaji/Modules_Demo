edd5a49908a3d26c0841697be5253e901d86eae7
DanishHaji <danish.haji111@gmail.com>
2024-07-14 02:00:08 +0500

    Updated Code

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-
+/*
 
 import add from "./index.js"; // function with default export and import
 console.log(add(5,5));
@@ -6,9 +6,13 @@ console.log(add(5,5));
 import { userNames } from "./index.js"; // array with direct export and import
 console.log(userNames);
 
+// import {userName , favColor} from "./index.js"; // we can write it like this too to avoid time waste and space.
+
 import { favColor } from "./index.js"; // variable with direct export and import
 console.log(favColor);
 
 
 import { myBio as bio } from "./index.js"; // object with direct export and import (using as keyword to declare new name of variable and then will be console with new name)
 console.log(bio);
+
+*/
--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
+/*
+
 export default add    // (export = to make module ) (default = to make module default if its important) (add = name of function or any variable)
 function add (num1:number , num2:number) {
         return num1 + num2
 };
 
 export let userNames:string[] = ["Ali","Usman", "Danish"] // (Direct export with code)
+
+// export {userName , favColor}; // (Direct export with code) (we can also write it like this to avoid time waste and space.)
 
 export let favColor = "Red" // (Direct export with code)
 
@@ -12,3 +16,5 @@ export let myBio = {  // (Direct export with code)
     age:32,
     gender:"Male",
 };
+
+*/
